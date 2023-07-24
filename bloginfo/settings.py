5687dc518794d22bb71768c5a06cd6d2b1d13974
Bounama Kounta <bounamakountagoudiaby@gmail.com>
2023-07-24 12:32:50 +0100

    config dba1

--- bloginfo/settings.py
+++ bloginfo/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = "bloginfo.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-#DATABASES = {
- #   "default": {
-  #      "ENGINE": "django.db.backends.sqlite3",
-  #      "NAME": BASE_DIR / "db.sqlite3",
-  #  }
-#}
-
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST":"127.0.0.1",
-        "USER":"postgres",
-        "NAME":"codelive",
-        "PASSWORD":"Postgre55555",
-        "PORT":"5432"
+       "ENGINE": "django.db.backends.sqlite3",
+       "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
+#DATABASES = {
+#    "default": {
+    #    "ENGINE": "django.db.backends.postgresql",
+ #       "HOST":"127.0.0.1",
+      #  "USER":"postgres",
+      #  "NAME":"codelive",
+     #   "PASSWORD":"Postgre55555",
+    #    "PORT":"5432"
+   # }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
